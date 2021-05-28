c3703b2e79186fd25288aef8e93806f9063ce8f5
umerkang66 <umerkang66@gmail.com>
2021-05-28 13:00:24 +0500

    added header component, links to home, shop and contact page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.scss';
 import { Route, Switch } from 'react-router-dom';
+
+import Header from './components/header/Header.js';
 import HomePage from './pages/homepage/HomePage.js';
 import ShopPage from './pages/shop/Shop.js';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route exact path="/shop" component={ShopPage}></Route>
--- src/components/collectionPreview/PreviewCollection.js
+++ src/components/collectionPreview/PreviewCollection.js
@@ -4,8 +4,6 @@ import './CollectionPreview.scss';
 import CollectionItem from '../collectionItem/CollectionItem.js';
 
 const PreviewCollection = ({ title, items, routeName }) => {
-  console.log(routeName);
-
   return (
     <div style={{ color: 'white' }} className="collection-preview">
       <h1 className="title">{title.toUpperCase()}</h1>
