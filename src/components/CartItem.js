179ed0d74ee6fe7ad0d9d3a79716fbd49f5bb8aa
umerkang66 <umerkang66@gmail.com>
2021-07-10 21:32:55 +0500

    added react memo to the cartitem component and changed some minor menuitem styling

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 import '../sass/components/CartItem.scss';
 
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
@@ -15,4 +15,4 @@ const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
   );
 };
 
-export default CartItem;
+export default memo(CartItem);
